bd7b4913bc81768e496b0b3d9589fed7be8087a2
seonhyeokjun <tjsrmatn@naver.com>
2022-01-04 00:01:13 +0900

    스프링 공부

---
+++ src/main/java/com/hello/hellospring/service/MemberService.java
@@ -0,0 +1,46 @@
+package com.hello.hellospring.service;
+
+import com.hello.hellospring.domain.Member;
+import com.hello.hellospring.repository.MemberRepository;
+
+import java.util.*;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원 가입
+     * @param member
+     * @return
+     */
+    public Long join(Member member){
+        validateDuplicateMember(member); // 중복회원 검증
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다");
+        });
+    }
+
+    /**
+     * 전체 회원 조회
+     * @return
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
+
