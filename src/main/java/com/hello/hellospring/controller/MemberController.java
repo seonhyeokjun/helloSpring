15ee63f3a836ed6e68038f94f410f279414d5efc
seonhyeokjun <tjsrmatn@naver.com>
2022-01-05 00:53:28 +0900

    직접 빈 등록, 스프링 통합 테스트, H2, jdbcTemplate

--- src/main/java/com/hello/hellospring/controller/MemberController.java
+++ src/main/java/com/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package com.hello.hellospring.controller;
 
+import com.hello.hellospring.domain.Member;
 import com.hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.*;
 
 @Controller
 public class MemberController {
@@ -13,5 +19,25 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
 
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
--- src/main/java/com/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/com/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,6 +1,7 @@
 package com.hello.hellospring.repository;
 
 import com.hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
